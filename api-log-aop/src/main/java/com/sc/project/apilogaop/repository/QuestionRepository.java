56c35af8a5a88d409c3b1a0b4599269f3e38f320
sc7258 <sc7258@users.noreply.github.com>
2024-12-18 11:30:28 +0900

    Delete api-log-aop directory

--- api-log-aop/src/main/java/com/sc/project/apilogaop/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sc.project.apilogaop.repository;
-
-import com.sc.project.apilogaop.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
